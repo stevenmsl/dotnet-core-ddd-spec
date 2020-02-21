9b06cb27fe8133c52e90c1c35af0927e44405475
Steven Lin <slin@merchantgrowth.com>
2020-02-21 15:22:47 -0800

    add ‘or’ and ‘not’ specs

--- Lib/SeedWork/Specification.cs
+++ Lib/SeedWork/Specification.cs
@@ -22,6 +22,11 @@ namespace Lib
             _left = left;
         }
 
+        /*
+         *  So the idea here is to create an expression that would invoke 
+         *  the left and right expressions with the same parameters and then 
+         *  apply && operator to combine these two results.
+         */
         public override Expression<Func<T, bool>> ToExpression()
         {
             var left = _left.ToExpression();
@@ -29,12 +34,93 @@ namespace Lib
 
             var invoked = Expression.Invoke(right, left.Parameters);
 
-            return
-                (Expression < Func<T, bool> >) Expression.Lambda                    
-                    (Expression.AndAlso(left.Body, invoked), left.Parameters);                                
+            /* This is what it would look like after applying the AndAlso operator 
+             * 
+               .Lambda #Lambda1<System.Func`2[UnitTest.TwoNumbers,System.Boolean]>(UnitTest.TwoNumbers $twoNumbers) {
+               $twoNumbers.First + $twoNumbers.Second <= 1000 && .Invoke (.Lambda #Lambda2<System.Func`2[UnitTest.TwoNumbers,System.Boolean]>)($twoNumbers)
+               }
+
+               .Lambda #Lambda2<System.Func`2[UnitTest.TwoNumbers,System.Boolean]>(UnitTest.TwoNumbers $twoNumbers) {
+                    $twoNumbers.First + $twoNumbers.Second >= 10
+               }                           
+            */
+
+            var combined =  (Expression < Func<T, bool> >) Expression.Lambda                    
+                    (Expression.AndAlso(left.Body, invoked), left.Parameters);
+
+            return combined;
         }
 
+            
+               
+
     }
+
+    internal sealed class OrSpecification<T> : Specification<T>
+    {
+        private readonly Specification<T> _left;
+        private readonly Specification<T> _right;
+        public OrSpecification(Specification<T> left, Specification<T> right)
+        {
+            _right = right;
+            _left = left;
+        }
+
+        public override Expression<Func<T, bool>> ToExpression()
+        {
+            var left = _left.ToExpression();
+            var right = _right.ToExpression();
+
+            var invoked = Expression.Invoke(right, left.Parameters);
+
+            /* This is what it would look like after applying the AndAlso operator 
+             * 
+            .Lambda #Lambda1<System.Func`2[UnitTest.TwoNumbers,System.Boolean]>(UnitTest.TwoNumbers $twoNumbers) {
+                $twoNumbers.First + $twoNumbers.Second <= 1000 || .Invoke (.Lambda #Lambda2<System.Func`2[UnitTest.TwoNumbers,System.Boolean]>)($twoNumbers)
+            }
+
+            .Lambda #Lambda2<System.Func`2[UnitTest.TwoNumbers,System.Boolean]>(UnitTest.TwoNumbers $twoNumbers) {
+                $twoNumbers.First + $twoNumbers.Second >= 10
+            }                        
+            */
+
+            var combined = (Expression<Func<T, bool>>)Expression.Lambda
+                    (Expression.OrElse(left.Body, invoked), left.Parameters);
+
+            return combined;
+        }
+    }
+
+    internal sealed class NotSpecification<T> : Specification<T>
+    {
+        private readonly Specification<T> _spec;
+        
+        public NotSpecification(Specification<T> spec)
+        {
+            _spec = spec;
+        }
+
+        public override Expression<Func<T, bool>> ToExpression()
+        {
+            var expr = _spec.ToExpression();
+            var notExpr = Expression.Not(expr.Body);
+
+            /*
+             * 
+             * .Lambda #Lambda1<System.Func`2[UnitTest.TwoNumbers,System.Boolean]>(UnitTest.TwoNumbers $twoNumbers) {
+               !($twoNumbers.First + $twoNumbers.Second <= 1000)
+               */
+            var converted = Expression.Lambda<Func<T, bool>>(notExpr, expr.Parameters);
+            return converted;
+
+
+        }
+
+        
+    }
+
+
+
     #endregion
 
     // this is the only class type exposed to the client code
@@ -59,6 +145,20 @@ namespace Lib
             if (spec == All) return this;
             return new AndSpecification<T>(this, spec);
         }
+
+        public Specification<T> Or(Specification<T> spec)
+        {
+            // no further evaluation is required
+            // as the result will be always true
+            if (this == All || spec == All) return All;
+            return new OrSpecification<T>(this, spec);
+        }
+
+        public Specification<T> Not()
+        {
+            return new NotSpecification<T>(this);
+        } 
+
 
     }
 }
--- UnitTest/UnitTestMain.cs
+++ UnitTest/UnitTestMain.cs
@@ -7,7 +7,7 @@ namespace UnitTest
     public class UnitTestMain
     {
         [Fact]
-        public void TestSpec()
+        public void TestAndSpec()
         {
             var twoNumbers = new TwoNumbers(10, 20);
             var checkUpper = new UpperLimitSpec();
@@ -15,6 +15,26 @@ namespace UnitTest
             var checkBoth = checkUpper.And(checkLower);
             Assert.True(checkBoth.IsSatisfiedBy(twoNumbers));
         }
+
+        [Fact]
+        public void TestOrSpec()
+        {
+            var twoNumbers = new TwoNumbers(1000, 20);
+            var checkUpper = new UpperLimitSpec();
+            var checkLower = new LowerLimitSpec();
+            var checkBoth = checkUpper.Or(checkLower);
+            Assert.True(checkBoth.IsSatisfiedBy(twoNumbers));
+        }
+
+        [Fact] 
+        public void TestNotSpec()
+        {
+            var twoNumbers = new TwoNumbers(1000, 20);
+            var checkUpper = new UpperLimitSpec();
+            var checkNot = checkUpper.Not();
+            Assert.True(checkNot.IsSatisfiedBy(twoNumbers));
+        }
+
     }
 
 
